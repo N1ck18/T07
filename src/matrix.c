efdadca42c3956e7359800b3f3e6bd3b224c22a2
N1ck18 <nick18-2006@rambler.ru>
2024-08-06 22:12:21 +0700

    init

---
+++ src/matrix.c
@@ -0,0 +1,165 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NMAX 100
+
+int input_menu(int *menu);
+int input_matrix_size(int *x, int *y);
+int input_static_matrix(int x, int y, int matrix[x][y]);
+int input_dinamic_matrix(int x, int y, int **matrix);
+void output_static_matrix(int x, int y, int matrix[x][y]);
+void output_dinamic_matrix(int x, int y, int **matrix);
+
+int **create_single_array_matrix(int x, int y);
+int **create_two_array_matrix(int x, int y);
+int **create_slow_matrix(int x, int y);
+
+int main() {
+    int result = 1;
+    int menu = 0;
+    int x = 0, y = 0;
+
+    if (!input_menu(&menu) || !input_matrix_size(&x, &y)) {
+        result = 0;
+    }
+    if (result != 0) {
+        if (menu == 1) {
+            int data[x][y];
+            if (x <= NMAX && y <= NMAX && input_static_matrix(x, y, data)) {
+                output_static_matrix(x, y, data);
+            } else
+                result = 0;
+        } else if (menu == 2) {
+            int **data = create_single_array_matrix(x, y);
+            if (input_dinamic_matrix(x, y, data)) {
+                output_dinamic_matrix(x, y, data);
+            } else
+                result = 0;
+            free(data);
+        } else if (menu == 3) {
+            int **data = create_two_array_matrix(x, y);
+            if (input_dinamic_matrix(x, y, data)) {
+                output_dinamic_matrix(x, y, data);
+            } else
+                result = 0;
+            free(*data);
+            free(data);
+        } else {
+            int **data = create_slow_matrix(x, y);
+            if (input_dinamic_matrix(x, y, data)) {
+                output_dinamic_matrix(x, y, data);
+            } else
+                result = 0;
+            for (int i = 0; i < y; i++) {
+                free(data[i]);
+            }
+            free(data);
+        }
+    }
+    if (result == 0) printf("n/a");
+
+    return 0;
+}
+
+int **create_two_array_matrix(int x, int y) {
+    int *data = malloc(sizeof(int) * x * y);
+    int **ptr = (int **)malloc(sizeof(int) * y);
+    for (int i = 0; i < y; i++) {
+        *(ptr + i) = data + x * i;
+    }
+    return ptr;
+}
+
+int **create_slow_matrix(int x, int y) {
+    int **ptr = malloc(sizeof(int *) * y);
+    for (int i = 0; i < y; i++) {
+        *(ptr + i) = malloc(sizeof(int) * x);
+    }
+    return ptr;
+}
+
+int **create_single_array_matrix(int x, int y) {
+    int **data = (int **)malloc(sizeof(int) * x * y + y * sizeof(int *));
+    int *ptr = (int *)(data + y);
+    for (int i = 0; i < y; i++) {
+        data[i] = ptr + i * x;
+    }
+    return data;
+}
+
+void output_static_matrix(int x, int y, int matrix[x][y]) {
+    for (int i = 0; i < y; i++) {
+        for (int j = 0; j < x; j++) {
+            printf("%d", matrix[i][j]);
+            if (j != x - 1) {
+                printf(" ");
+            }
+        }
+        if (i != y - 1) {
+            printf("\n");
+        }
+    }
+}
+
+int input_menu(int *menu) {
+    int result = 1;
+    char ch;
+    if (scanf("%d", menu) != 1 || *menu < 1 || *menu > 4 || (ch = getchar()) != '\n') {
+        result = 0;
+    }
+    return result;
+}
+
+int input_matrix_size(int *x, int *y) {
+    int result = 1;
+    char ch;
+    if (scanf("%d %d", x, y) != 2 || *x <= 0 || *y <= 0 || ((ch = getchar()) != '\n')) {
+        result = 0;
+    }
+    return result;
+}
+
+int input_static_matrix(int x, int y, int matrix[x][y]) {
+    int result = 1;
+    for (int i = 0; i < y; i++) {
+        for (int j = 0; j < x; j++) {
+            if (scanf("%d", &matrix[i][j]) != 1) {
+                result = 0;
+            }
+        }
+    }
+    if (getchar() != '\n') {
+        result = 0;
+    }
+    return result;
+}
+
+int input_dinamic_matrix(int x, int y, int **matrix) {
+    int result = 1;
+    for (int i = 0; i < y; i++) {
+        for (int j = 0; j < x; j++) {
+            if (scanf("%d", &matrix[i][j]) != 1) {
+                result = 0;
+            }
+        }
+    }
+    if (getchar() != '\n') {
+        result = 0;
+    }
+    return result;
+}
+
+void output_dinamic_matrix(int x, int y, int **matrix) {
+    for (int i = 0; i < y; i++) {
+        for (int j = 0; j < x; j++) {
+            printf("%d", matrix[i][j]);
+            if (j != x - 1) {
+                printf(" ");
+            }
+        }
+        if (i != y - 1) {
+            printf("\n");
+        }
+    }
+}
---
+++ src/sort_no_leak.c
@@ -0,0 +1,69 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NMAX 10
+
+int input_array(int a[], int *n);
+void output(int a[], int n);
+void sort(int *a, int n);
+
+int main() {
+    int n = 0;
+    int *data = NULL;
+    char ch;
+    if (scanf("%d", &n) && ((ch = getchar()) == ' ' || ch == '\n') && (n > 0 && n <= NMAX)) {
+        data = (int *)malloc(n * sizeof(int));
+        if (input(data, &n)) {
+            sort(data, n);
+            output(data, n);
+        } else {
+            printf("n/a");
+        }
+    } else {
+        printf("n/a");
+    }
+    free(data);
+    return 0;
+}
+void sort(int *a, int n) {
+    for (int j = 0; j < n; j++) {
+        int move = 0;
+        for (int i = 1; i < n; i++) {
+            if (*(a + i - 1) > *(a + i)) {
+                int temp = *(a + i);
+                *(a + i) = *(a + i - 1);
+                *(a + i - 1) = temp;
+                move = 1;
+            }
+        }
+        if (move == 0) {
+            break;
+        }
+    }
+    return;
+}
+
+int input_array(int a[], int *n) {
+    int result = 1;
+    for (int *p = a; p - a < *n; p++) {
+        if (scanf("%d", p) != 1) {
+            result = 0;
+            break;
+        }
+    }
+    if (result != 0 && getchar() != '\n') {
+        result = 0;
+    }
+
+    return result;
+}
+
+void output(int a[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d", a[i]);
+        if (i != n - 1) {
+            printf(" ");
+        }
+    }
+}
